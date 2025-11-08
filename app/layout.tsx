65b4800fd32e274e275f0f3224908d8a8005fa0e
Svitlana Shumal <svetlankachajka@gmail.com>
2025-11-08 08:26:40 +0200

    Added favicon and file with symbol-defs.svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,45 @@
-import type { Metadata } from "next";
-import { Inter, Nunito_Sans } from "next/font/google";
-import "modern-normalize/modern-normalize.css";
-import "./globals.css";
-import Header from "@/components/Header/Header";
+import type { Metadata } from 'next';
+import { Inter, Nunito_Sans } from 'next/font/google';
+import 'modern-normalize/modern-normalize.css';
+import './globals.css';
+import Header from '@/components/Header/Header';
 // import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
-import Footer from "@/components/Footer/Footer";
+import Footer from '@/components/Footer/Footer';
 // import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
 const inter = Inter({
-  subsets: ["cyrillic"],
-  weight: ["400", "500", "600"],
-  variable: "--font-inter",
-  display: "swap",
+  subsets: ['cyrillic'],
+  weight: ['400', '500', '600'],
+  variable: '--font-inter',
+  display: 'swap',
 });
 const nunito = Nunito_Sans({
-  subsets: ["cyrillic"],
-  weight: ["400"],
-  variable: "--font-nunito-sans",
-  display: "swap",
+  subsets: ['cyrillic'],
+  weight: ['400'],
+  variable: '--font-nunito-sans',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "Welcome to Dream Cloth Market",
-  description:
-    " Dream is a simple and efficient application for buyimg your favourite clothes.",
+  title: 'Welcome to Dream Cloth Market',
+  description: ' Dream is a simple and efficient application for buyimg your favourite clothes.',
+  icons: { icon: '/Favicon.png' },
   openGraph: {
-    title: "Welcome to Dream Cloth Market",
-    description:
-      " Dream is a simple and efficient application for buyimg your favourite clothes.",
+    title: 'Welcome to Dream Cloth Market',
+    description: ' Dream is a simple and efficient application for buyimg your favourite clothes.',
     images: [
       {
-        url: "https://08-zustand-phi-bice.vercel.app/",
+        url: 'https://08-zustand-phi-bice.vercel.app/',
         width: 1200,
         height: 630,
-        alt: "Welcome to Dream Cloth Market",
+        alt: 'Welcome to Dream Cloth Market',
       },
     ],
-    url: "https://08-zustand-phi-bice.vercel.app/",
+    url: 'https://08-zustand-phi-bice.vercel.app/',
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       {/* <body className={`${inter.variable} ${nunito.variable} `}>
