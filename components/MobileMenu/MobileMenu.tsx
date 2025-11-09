ecd94c0e583c5c67aae6cbb5f7d4854065e4b1d5
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-11-09 22:24:51 +0100

    add header + mobile menu
    - add styles
    - add authNavigation
    - create mob menu

--- components/MobileMenu/MobileMenu.tsx
+++ components/MobileMenu/MobileMenu.tsx
@@ -1,23 +1,56 @@
 import Link from 'next/link';
 import AuthNavigation from '../AuthNavigation/AuthNavigation';
+import css from './MobileMenu.module.css';
 
-export default function MobileMenu({ onClose }: { onClose: () => void }) {
+export default function MobileMenu({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
   return (
-    <div>
-      <button onClick={onClose}>✕</button>
-      <Link href="/" onClick={onClose}>
-        Головна
-      </Link>
-      <Link href="/goods" onClick={onClose}>
-        Товари
-      </Link>
-      <Link href="/categories" onClick={onClose}>
-        Категорії
-      </Link>
-      <AuthNavigation />
-      <Link href="/cart" onClick={onClose}>
-        Кошик
-      </Link>
+    <div className={css.container}>
+      <div className={`${css.mobileMenu} ${isOpen ? css.isOpen : ''}`}>
+        <div className={css.mobileMenuHeader}>
+          <Link href="/">
+            Clothica
+            <svg width="84" height="36">
+              <use href="/public/symbol-defs.svg/#" />
+            </svg>
+          </Link>
+
+          <div className={css.mobileMenuNavigationBtn}>
+            <button className={css.mobileMenuCloseBtn} onClick={onClose}>
+              X
+              {/* <svg className={css.mobileMenuCloseBtnIcon} width="24" height="24">
+                      <use href="/public/symbol-defs.svg#icon-close" />
+                    </svg> */}
+            </button>
+            <Link className={css.mobileMenuBasket} href="/order" onClick={onClose}>
+              <svg className={css.mobileMenuCloseBtnIcon} width="36" height="36">
+                <use href="/public/symbol-defs.svg#icon-close" />
+              </svg>
+            </Link>
+          </div>
+        </div>
+
+        <div className={css.mobileMenuListContainer}>
+          <ul className={css.mobileMenuList}>
+            <li className={css.mobileMenuItem}>
+              <Link className={css.mobileMenuLink} href="/" onClick={onClose}>
+                Головна
+              </Link>
+            </li>
+            <li className={css.mobileMenuItem}>
+              <Link className={css.mobileMenuLink} href="/goods" onClick={onClose}>
+                Товари
+              </Link>
+            </li>
+            <li className={css.mobileMenuItem}>
+              <Link className={css.mobileMenuLink} href="/categories" onClick={onClose}>
+                Категорії
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <AuthNavigation variant="mobile" />
+      </div>
     </div>
   );
 }
