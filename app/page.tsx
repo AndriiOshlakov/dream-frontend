5aefac3417ae91d7849b65ab339c39bd7c9d905f
oleksandrboo77 <oleksandrboo77@gmail.com>
2025-11-12 00:25:43 +0200

    add buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import css from '@/app/Home.module.css';
+import ReviewsList from '@/components/ReviewsList/ReviewsList';
 
 export default function HomePage() {
   return (
     <div>
       <h1>Main Page</h1>
+      <ReviewsList />
     </div>
   );
 }
