41cdcfb2f565a433a8c0755258e554a8b18acfc0
Oleksandra Ovcharova <oleksaovcharova@gmail.com>
2025-11-12 17:48:37 +0100

    Profile page.

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,3 +1,18 @@
-import css from './ProfileOrderPage.module.css';
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { fetchMyOrders } from '@/lib/api/serverApi';
+import ProfileClient from './Profile.client';
 
-export default function Profile() {}
+export default async function Profile() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['myOrders'],
+    queryFn: fetchMyOrders,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProfileClient />
+    </HydrationBoundary>
+  );
+}
--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -10,3 +10,24 @@ export const checkServerSession = async () => {
   });
   return response;
 };
+
+export const fetchMyOrders = async () => {
+  return [
+    {
+      _id: '69135246d77bd9a8f9389459',
+      userId: '64f5b1c8b6e1f9a123456789',
+      goods: [
+        {
+          productId: '671fbb85ec7f5f1d6b65f2a9',
+          title: 'Сорочка чоловіча',
+          quantity: 2,
+          price: 850,
+          total: 1700,
+        },
+      ],
+      status: 'pending',
+      createdAt: '2025-11-11T21:32:57.327Z',
+      updatedAt: '2025-11-11T21:32:57.327Z',
+    },
+  ];
+};
