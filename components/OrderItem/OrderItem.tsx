eea3bf1e4a44615a0d923810f2fd42e4420691f9
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-11-12 21:12:55 +0200

    order item comment

--- components/OrderItem/OrderItem.tsx
+++ components/OrderItem/OrderItem.tsx
@@ -1,6 +1,6 @@
 import css from './OrderItem.module.css';
 
-function formatDate(iso) {
+function formatDate(iso: string | Date) {
   try {
     const d = new Date(iso);
     return d.toLocaleDateString('uk-UA');
@@ -9,25 +9,25 @@ function formatDate(iso) {
   }
 }
 
-export default function OrderItem({ order }) {
-  const total = order.goods.reduce((sum, good) => sum + good.total, 0);
+// export default function OrderItem({ order }) {
+//   const total = order.goods.reduce((sum, good) => sum + good.total, 0);
 
-  return (
-    <div className={css.itemContainer}>
-      <div>
-        <time dateTime={order.createdAt}>{formatDate(order.createdAt)}</time>
-        <div className={css.label}>#{order._id.slice(0, 8)}</div>
-      </div>
+//   return (
+//     <div className={css.itemContainer}>
+//       <div>
+//         <time dateTime={order.createdAt}>{formatDate(order.createdAt)}</time>
+//         <div className={css.label}>#{order._id.slice(0, 8)}</div>
+//       </div>
 
-      <div>
-        <div className={css.label}>Сума:</div>
-        <div className={css.value}>{total} грн</div>
-      </div>
+//       <div>
+//         <div className={css.label}>Сума:</div>
+//         <div className={css.value}>{total} грн</div>
+//       </div>
 
-      <div>
-        <div className={css.label}>Статус:</div>
-        <div className={css.value}>В обробці</div>
-      </div>
-    </div>
-  );
-}
+//       <div>
+//         <div className={css.label}>Статус:</div>
+//         <div className={css.value}>В обробці</div>
+//       </div>
+//     </div>
+//   );
+// }
