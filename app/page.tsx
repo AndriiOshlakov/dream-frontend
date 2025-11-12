a3ae98951b804d5d356e8b93c636a62c7f72e436
MaryanaRashkevych <149114863+MaryanaRashkevych@users.noreply.github.com>
2025-11-12 09:37:26 +0200

    create order initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import css from '@/app/Home.module.css';
 import Hero from '@/components/Hero/page';
 import Style from '@/components/Style/page';
+import CreateOrder from './order/page';
 
 export default function HomePage() {
   return (
     <>
      <Hero/>
      <Style/>
+     <CreateOrder/>
     </>
   );
 }
