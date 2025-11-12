eb02f94a73a3acc0cfba876cd8929def92537c00
Svitlana Shumal <svetlankachajka@gmail.com>
2025-11-12 17:41:18 +0200

    fixed footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import css from '@/app/Home.module.css';
 import Hero from '@/components/Hero/page';
 import Style from '@/components/Style/page';
 import ReviewsList from '@/components/ReviewsList/ReviewsList';
-        
+
 export default function HomePage() {
   return (
     <>
-     <Hero/>
-     <Style/>
+      <Hero />
+      <Style />
       <ReviewsList />
+    </>
   );
 }
