ed17ec5a10ed5f192f995e563624a7fdb7ff94c3
oleksiirudenko <or190802@gmail.com>
2025-11-13 21:40:01 +0200

    Add route handlers for api

---
+++ app/api/api.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosError } from 'axios';
+
+export type ApiError = AxiosError<{ error: string }>;
+
+export const api = axios.create({
+  baseURL: 'https://dream-backend-a69s.onrender.com/api',
+  withCredentials: true,
+});
